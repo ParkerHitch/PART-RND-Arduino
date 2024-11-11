4bcaddffa915094cac1de658a48df19a467ea45e
ParkerHitch <parker.hitchcock@gmail.com>
2024-11-11 18:22:08 -0500

    feat: Good testing stuff

--- FA24_ESC_Setup/FA24_ESC_Setup.ino
+++ FA24_ESC_Setup/FA24_ESC_Setup.ino
@@ -7,30 +7,35 @@
 #define CALIB_READING 219252
 #define CALIB_KNOWN 5
 
-#define ESC_PIN 8
+#define ESC_PIN 9
+#define SERVO_PIN 10
 
 #define LOAD_SCK_PIN 4
 #define LOAD_DT_PIN 5
 
-Servo servo;  //esc
+Servo esc;  //esc
+Servo servo;
 HX711 load;
 
 int pwmMin = 1100;
 int pwmMax = 1900;
 int status = 0;
 int speedPercent = 0;
+int counter = 0;
 double reading;
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(ESC_PIN, pwmMin, pwmMax);
+  servo.attach(SERVO_PIN, pwmMin, pwmMax);
+  esc.attach(ESC_PIN, pwmMin, pwmMax);
   load.begin(LOAD_DT_PIN, LOAD_SCK_PIN, 0);
-  servo.writeMicroseconds(pwmMin);
+  esc.writeMicroseconds(pwmMin);
 
   Serial.println("Enter starting speed: ");
 }
 
 void loop() {
+  servo.write(52);
   if (Serial.available()) {
     int nextChar = Serial.read();
     if (nextChar == 'P') {
@@ -51,22 +56,30 @@ void loop() {
     }
   }
 
-  if (load.is_ready()) {
-    load.set_scale(1);
-    reading = load.get_units(1);
+  counter++;
+  if (counter == 10){
+    counter = 0;
+    if (load.is_ready()) {
+      load.set_scale(1);
+      reading = load.get_units(1);
+    }
+
+    //Serial.print("Speed (%): ");
+    Serial.print(status);
+    Serial.print(", ");
+    //Serial.print("Load: ");
+    // For tilt rotor:
+    Serial.println(reading / 102842);
+    // For back:
+    // Serial.println(reading / 96775);
   }
 
-  //Serial.print("Speed (%): ");
-  Serial.print(status);
-  Serial.print(", ");
-  //Serial.print("Load: ");
-  Serial.println(reading / 96775);
-  delay(200);
+  delay(10);
 }
 
 int parkersFunction(int status) {
   speedPercent = ((pwmMax - pwmMin) / 100) * status + pwmMin;
-  servo.writeMicroseconds(speedPercent);
+  esc.writeMicroseconds(speedPercent);
   Serial.print("PWM set to ");
   Serial.print(speedPercent);
   Serial.println("%");
