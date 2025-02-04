d5fb0503168783cabd6f9c1f6632074755a686c5
ParkerHitch <parker.hitchcock@gmail.com>
2025-02-04 10:34:51 -0500

    feat: Add tilt servo support to testbench

--- FA24_ESC_Setup/FA24_ESC_Setup.ino
+++ FA24_ESC_Setup/FA24_ESC_Setup.ino
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include "HX711.h"
 
-#define IN_BUFF_SZ 20
-
-#define CALIB_READING 219252
-#define CALIB_KNOWN 5
+#define LOADCELL_TICKS_PER_KG 102842
+#define LOOP_DELAY_MS 10
 
 #define ESC_PIN 9
+#define ESC_PWM_MIN 1100
+#define ESC_PWM_MAX 1900
+
 #define SERVO_PIN 10
+#define SERVO_PWM_HORIZ 1100
+#define SERVO_PWM_VERT  1900
+
+#define SERVO_PWM_MAX (SERVO_PWM_VERT > SERVO_PWM_HORIZ ? SERVO_PWM_VERT : SERVO_PWM_HORIZ)
+#define SERVO_PWM_MIN (SERVO_PWM_VERT > SERVO_PWM_HORIZ ? SERVO_PWM_HORIZ : SERVO_PWM_VERT)
 
 #define LOAD_SCK_PIN 4
 #define LOAD_DT_PIN 5
@@ -17,32 +23,51 @@ Servo esc;  //esc
 Servo servo;
 HX711 load;
 
-int pwmMin = 1100;
-int pwmMax = 1900;
-int status = 0;
-int speedPercent = 0;
-int counter = 0;
+int currentEscPercent = 0;
+int currentServoAngle = 0;
+int readingCounter = 0;
 double reading;
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(SERVO_PIN, pwmMin, pwmMax);
-  esc.attach(ESC_PIN, pwmMin, pwmMax);
-  load.begin(LOAD_DT_PIN, LOAD_SCK_PIN, 0);
-  esc.writeMicroseconds(pwmMin);
 
-  Serial.println("Enter starting speed: ");
+  servo.attach(SERVO_PIN, SERVO_PWM_MIN, SERVO_PWM_MAX);
+  esc.attach(ESC_PIN, ESC_PWM_MIN, ESC_PWM_MAX);
+
+  load.begin(LOAD_DT_PIN, LOAD_SCK_PIN, 0);
+
+  esc.writeMicroseconds(ESC_PWM_MIN);
+  servo.writeMicroseconds(SERVO_PWM_VERT);
 }
 
 void loop() {
-  servo.write(52);
   if (Serial.available()) {
     int nextChar = Serial.read();
+    // Percent command:
+    //   Of form "Pxxx", where xxx = percent to go to
     if (nextChar == 'P') {
-      status = Serial.parseInt();
-      if (status <= 100 && status >= 0) {
-        speedPercent = parkersFunction(status);
+      currentEscPercent = Serial.parseInt();
+      if (currentEscPercent <= 100 && currentEscPercent >= 0) {
+        writePwmPercent(currentEscPercent);
       }
+    // Servo command, one of forms:
+    //    "Sxx", where xx = degrees. 0 = down, 90 = up
+    //    "SRxx", where xx = raw value for servo (in us). Should only be used for debugging
+    } else if (nextChar == 'S') {
+      int next = Serial.read();
+      
+      int newServoUs;
+
+      if (Serial.peek() == 'R') {
+        Serial.read();
+        newServoUs = Serial.parseInt();
+      } else {
+        currentServoAngle = Serial.parseInt();
+        newServoUs = ((SERVO_PWM_VERT - SERVO_PWM_HORIZ) * currentServoAngle / 90) + SERVO_PWM_HORIZ;
+      }
+      servo.writeMicroseconds(newServoUs);
+    // Tare command, of form:
+    //    "TARE", tares the load cell
     } else if (nextChar == 'T') {
       char buff[3] = {};
       size_t read = Serial.readBytes(buff, 3);
@@ -50,39 +75,35 @@ void loop() {
         // Tare time:
         load.tare();
       }
-    } else if (nextChar != 10) {
-      status = 0;
-      speedPercent = parkersFunction(status);
+    } else if (nextChar != '\n') {
+      currentEscPercent = 0;
+      writePwmPercent(currentEscPercent);
     }
   }
 
-  counter++;
-  if (counter == 10){
-    counter = 0;
+  readingCounter++;
+  if (readingCounter == 10){
+    readingCounter = 0;
     if (load.is_ready()) {
       load.set_scale(1);
       reading = load.get_units(1);
     }
 
-    //Serial.print("Speed (%): ");
-    Serial.print(status);
+    Serial.print(currentEscPercent);
     Serial.print(", ");
-    //Serial.print("Load: ");
-    // For tilt rotor:
-    Serial.println(reading / 102842);
-    // For back:
-    // Serial.println(reading / 96775);
+    Serial.print(reading / LOADCELL_TICKS_PER_KG);
+    Serial.print(", ");
+    Serial.println(currentServoAngle);
+
   }
 
-  delay(10);
+  delay(LOOP_DELAY_MS);
 }
 
-int parkersFunction(int status) {
-  speedPercent = ((pwmMax - pwmMin) / 100) * status + pwmMin;
-  esc.writeMicroseconds(speedPercent);
+void writePwmPercent(int percent) {
+  int new_us = ((ESC_PWM_MAX - ESC_PWM_MIN) * percent / 100) + ESC_PWM_MIN;
+  esc.writeMicroseconds(new_us);
   Serial.print("PWM set to ");
-  Serial.print(speedPercent);
-  Serial.println("%");
-
-  return (speedPercent);
+  Serial.print(new_us);
+  Serial.println("us");
 }
